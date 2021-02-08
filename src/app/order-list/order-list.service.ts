b882dada036e2c658c21acfddb0e16b5d2329a81
shekhar199511 <76092565+shekhar199511@users.noreply.github.com>
2021-02-08 12:41:42 +0530

    List size increase

--- src/app/order-list/order-list.service.ts
+++ src/app/order-list/order-list.service.ts
@@ -3,12 +3,21 @@ import { EventEmitter } from '@angular/core';
 
 export class OrderListService{
 
-    private orders = [{CustomerName : 'Rob', DeliveryAddress : 'Banaglore', NoOfItems: '5', Amount : '15', 
-                    TotalAmount : '75', Status : 'Ready To Serve'},
-                    {CustomerName : 'Danny', DeliveryAddress : 'Chennai', NoOfItems: '1', Amount : '15', 
-                    TotalAmount : '15', Status : 'Order Received'},
-                    {CustomerName : 'Leslie', DeliveryAddress : 'Hyderabad', NoOfItems: '2', Amount : '15', 
+    private orders = [{CustomerName : 'Arpita', DeliveryAddress : 'Karnataka', NoOfItems: '2', Amount : '30', 
+                    TotalAmount : '30', Status : 'Ready To Serve'},
+                    {CustomerName : 'Ankur', DeliveryAddress : 'Netherlands', NoOfItems: '3', Amount : '45', 
+                    TotalAmount : '45', Status : 'Order Received'},
+                    {CustomerName : 'Carlo', DeliveryAddress : 'Netherlands', NoOfItems: '3', Amount : '45', 
+                    TotalAmount : '45', Status : 'Order Received'},
+                    {CustomerName : 'Ganesh', DeliveryAddress : 'Netherlands', NoOfItems: '3', Amount : '45', 
+                    TotalAmount : '45', Status : 'Order Received'},
+                    {CustomerName : 'Kailash', DeliveryAddress : 'Madhya Pradesh', NoOfItems: '3', Amount : '45', 
+                    TotalAmount : '45', Status : 'Order Received'},
+                    {CustomerName : 'Mili', DeliveryAddress : 'Uttar Pradesh', NoOfItems: '2', Amount : '30', 
+                    TotalAmount : '30', Status : 'Order Received'},
+                    {CustomerName : 'Pooja', DeliveryAddress : 'Maharashtra', NoOfItems: '2', Amount : '30', 
                     TotalAmount : '30', Status : 'Preparing'}]; 
+  
 
     getOrders(){
         return this.orders.slice();
